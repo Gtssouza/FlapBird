d8056fd2ba7abbf782e28a5cfad5bbd9e90deb79
Gustavo Souza <gtssouza94@gmail.com>
2020-09-01 16:26:39 -0300

    Movimentando o pássaro

--- core/src/com/example/flapbird/Game.java
+++ core/src/com/example/flapbird/Game.java
@@ -7,21 +7,26 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
+	private int movimentoX = 0;
 	private SpriteBatch spriteBatch; //exibe imagens e formas dentro do jogo
 	private Texture passaro;
+	private Texture fundo;
 	
 	@Override
 	public void create () {
 		//Gdx.app.log("Create","Jogo iniciado");
 		spriteBatch = new SpriteBatch();
 		passaro = new Texture("passaro1.png");
+		fundo = new Texture("fundo.png");
 	}
 
 	@Override
 	public void render () {
 
+		movimentoX++;
 		spriteBatch.begin();
-		spriteBatch.draw(passaro,0,0);
+		spriteBatch.draw(fundo,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		spriteBatch.draw(passaro,movimentoX,500);
 		spriteBatch.end();
 
 		//Gdx.app.log("Render","Jogo renderizado");
