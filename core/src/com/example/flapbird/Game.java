ea129b63b88f530faa98ba0c5918b150f382621e
Gustavo Souza <gtssouza94@gmail.com>
2020-09-01 17:25:34 -0300

    Animando pássaro

--- core/src/com/example/flapbird/Game.java
+++ core/src/com/example/flapbird/Game.java
@@ -6,27 +6,40 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+
 public class Game extends ApplicationAdapter {
 	private int movimentoX = 0;
 	private SpriteBatch spriteBatch; //exibe imagens e formas dentro do jogo
-	private Texture passaro;
+	private Texture[] passaros;
 	private Texture fundo;
+
+	private float variacao = 0;
 	
 	@Override
 	public void create () {
 		//Gdx.app.log("Create","Jogo iniciado");
 		spriteBatch = new SpriteBatch();
-		passaro = new Texture("passaro1.png");
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+		passaros[1] = new Texture("passaro2.png");
+		passaros[2] = new Texture("passaro3.png");
 		fundo = new Texture("fundo.png");
 	}
 
 	@Override
 	public void render () {
 
-		movimentoX++;
+
 		spriteBatch.begin();
+		if(variacao > 3){
+			variacao=0;
+		}
 		spriteBatch.draw(fundo,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		spriteBatch.draw(passaro,movimentoX,500);
+		spriteBatch.draw(passaros[(int)variacao],movimentoX,500);
+		//Calcula a diferença entre um render e outro
+		variacao+= Gdx.graphics.getDeltaTime() * 10;
+		movimentoX++;
 		spriteBatch.end();
 
 		//Gdx.app.log("Render","Jogo renderizado");
