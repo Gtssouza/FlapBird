dc10d108b24b81442c6900ad10147651439c68b1
Gustavo Souza <gtssouza94@gmail.com>
2020-09-01 16:16:03 -0300

    Inserindo imagens

--- core/src/com/example/flapbird/Game.java
+++ core/src/com/example/flapbird/Game.java
@@ -7,27 +7,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch spriteBatch; //exibe imagens e formas dentro do jogo
+	private Texture passaro;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//Gdx.app.log("Create","Jogo iniciado");
+		spriteBatch = new SpriteBatch();
+		passaro = new Texture("passaro1.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		spriteBatch.begin();
+		spriteBatch.draw(passaro,0,0);
+		spriteBatch.end();
+
+		//Gdx.app.log("Render","Jogo renderizado");
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		//Gdx.app.log("Dispose","Descarte de conteudo");
 	}
 }
